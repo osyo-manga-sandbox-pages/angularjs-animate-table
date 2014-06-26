24d5b029753b4824e9c1c193ffcd5189bd729027
manga_osyo <manga.osyo@gmail.com>
2014-06-26 14:12:13 +0900

    Automated commit at 2014-06-26 05:12:13 UTC by middleman-deploy 0.2.4

---
+++ javascripts/all.js
@@ -0,0 +1,34 @@
+var AnimateDemoCtrl = function ($scope) {
+
+	$scope.months = [
+		"January",
+		"February",
+		"March",
+		"April",
+		"May",
+		"June",
+		"July",
+		"August",
+		"September",
+		"October",
+		"November",
+		"December",
+	]
+
+	$scope.click = function (){
+		$scope.col = []
+		for(var i=0 ; i < 10 ; i++){
+			if( Math.floor(Math.random() * 10 % 2) == 0){
+				$scope.col.push(i + 1 + ". " + $scope.months[i]);
+			}
+		}
+		$scope.row = []
+		for(var i=0 ; i < 10 ; i++){
+			if( Math.floor(Math.random() * 10 % 2) == 0){
+				$scope.row.push(i + 1 + ". " + $scope.months[i]);
+			}
+		}
+	}
+	$scope.click();
+
+};
